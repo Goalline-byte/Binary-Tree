5738a6a9374a20b1510450d380f89f5a12eb0ea1
rajputaradhna <aradhnatnsingh01@gmail.com>
2021-12-04 15:07:28 +0530

    intro about binary tree

---
+++ intro.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Intro{
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data,int left,int right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+
+    }
+       
+}
